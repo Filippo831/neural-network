1be10d5cbb7e31e0baf9e43d40b5c823fca2d83b
Filippo831 <filippoarduini83@gmail.com>
2025-11-17 16:02:07 +0100

    feat: reading images and labels batches

--- include/dataset.h
+++ include/dataset.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 #define IMAGE_HEADER_SIZE 16
-#define LABER_HEADER_SIZE 8
+#define LABEL_HEADER_SIZE 8
 
-typedef struct DatasetMetadata{
+typedef struct DatasetMetadata {
     uint32_t length;
     uint32_t rowSize;
     uint32_t colSize;
@@ -30,12 +30,53 @@ typedef struct DatasetMetadata{
  * 16 - ***         Pixel data         Each pixel is an unsigned value (0-255)
  *
  * LABEL DATASET
- * Offset (Bytes)        Content                    Description             
- * ---------------- ------------------- ----------------------------------- 
- * 0 - 3             Magic number        2049 (0x801 in hex)                
- * 4 - 7             Number of labels    Total labels in the dataset        
- * 8 - ***           Label Data          Each label is a single byte (0-9)  
+ * Offset (Bytes)        Content                    Description
+ * ---------------- ------------------- -----------------------------------
+ * 0 - 3             Magic number        2049 (0x801 in hex)
+ * 4 - 7             Number of labels    Total labels in the dataset
+ * 8 - ***           Label Data          Each label is a single byte (0-9)
  *
  *
  */
 DatasetMetadata *readMetadata(FILE *_imageFile, FILE *_labelFile);
+
+/*
+ * @param
+ * _batchSize: how many images there are per _batch
+ * _batchIndex: get the offset from where to start reading the images
+ * _imageFile: the file from where read the images
+ *
+ * @body
+ * read the images and store them inside an array of array
+ * the array will have size _bathSize and in every position there is a pointer
+ * to another array with size 28*28*1 bytes = 784
+ *
+ * @return
+ * return the array with the images written, the array is stored in the heap
+ * if the amound of byte read is less than the batch size write a white pixel as
+ * last value to signal that it happened
+ *
+ */
+
+// TODO: maybe insert a metadata variable as input to make the sizes of the
+// images variable if using a different dataset
+uint8_t *readImageBatch(size_t _batchSize, uint32_t _batchIndex,
+                        FILE *_imageFile);
+
+/*
+ * @param
+ * _batchSize: how many images there are per _batch
+ * _batchIndex: get the offset from where to start reading the images
+ * _labelFile: the file from where read the labels
+ *
+ * @body
+ * read the labels and write them into an array written on the heap. Every label
+ * is 1 byte long
+ *
+ * @return
+ * return the array where the labels are written
+ *
+ */
+
+uint8_t *readLabelBatch(size_t _batchSize, uint32_t _batchIndex,
+                        FILE *_labelFile);
--- src/dataset.c
+++ src/dataset.c
@@ -11,25 +11,83 @@ DatasetMetadata *readMetadata(FILE *_imageFile, FILE *_labelFile) {
 
     // _imageFile
     // read the bytes from 0 to 15
-    uint8_t bytesPerChunk = 4;
-    uint32_t imageData[IMAGE_HEADER_SIZE / bytesPerChunk];
-    // FIX: change the endianess of the reading somehow
-    size_t readBytes = fread(imageData, sizeof(imageData[0]),
-                             IMAGE_HEADER_SIZE / bytesPerChunk, _imageFile);
+    uint8_t imageData[IMAGE_HEADER_SIZE];
 
-    assert(readBytes == IMAGE_HEADER_SIZE / bytesPerChunk);
+    size_t readBytes =
+        fread(imageData, sizeof(imageData[0]), IMAGE_HEADER_SIZE, _imageFile);
+
+    assert(readBytes == IMAGE_HEADER_SIZE);
 
     // check that the bytes 0-3 contains the value 2051 (0x803 in hex)
-    printf("numero magico: %b\n", imageData[0]);
-    assert(imageData[0] == 0x803);
+    assert(imageData[0] == 0 && imageData[1] == 0 &&
+           imageData[2] == (0x803 & 0xff00) >> 8 &&
+           imageData[3] == (0x803 & 0xff));
+
     // get the number of images written in bytes from 4-7
-    metadata->length = imageData[1];
+    uint32_t length = imageData[7] + (imageData[6] << 8) + (imageData[5] << 8) +
+                      (imageData[4] << 8);
+    metadata->length = length;
+
     // get number of pixel in row written in bytes from 8-11
-    metadata->rowSize = imageData[2];
-    assert(imageData[2] == 28);
+    metadata->rowSize = imageData[11];
+    assert(imageData[11] == 28);
+
     // get number of pixel in column written in bytes from 12-15
-    metadata->colSize = imageData[3];
-    assert(imageData[3] == 28);
+    metadata->colSize = imageData[15];
+    assert(imageData[15] == 28);
+
+    // label file
+    uint8_t labelData[LABEL_HEADER_SIZE];
+    readBytes =
+        fread(imageData, sizeof(labelData[0]), LABEL_HEADER_SIZE, _labelFile);
+
+    assert(readBytes == LABEL_HEADER_SIZE);
+
+    // check that the bytes 0-3 contains the value 2049 (0x801 in hex)
+    assert(imageData[0] == 0 && imageData[1] == 0 &&
+           imageData[2] == (0x801 & 0xff00) >> 8 &&
+           imageData[3] == (0x801 & 0xff));
+
+    // get the number of images written in bytes from 4-7
+    length = imageData[7] + (imageData[6] << 8) + (imageData[5] << 8) +
+             (imageData[4] << 8);
+
+    // check if it's equal to the previous found value
+    assert(length == metadata->length);
 
     return metadata;
+}
+
+uint8_t *readImageBatch(size_t _batchSize, uint32_t _batchIndex,
+                        FILE *_imageFile) {
+    // allocate the array where the images batch will be stored
+    uint8_t *images = malloc(sizeof(uint8_t) * 28 * 28 * _batchSize);
+
+    // move forward by the header plus the batchSize * 28*28 (28*28 bytes per
+    // image) * _batchIndex
+    fseek(_imageFile, IMAGE_HEADER_SIZE + _batchSize * 28 * 28 * _batchIndex,
+          SEEK_SET);
+
+    size_t bytesRead =
+        fread(images, sizeof(uint8_t), _batchSize * 28 * 28, _imageFile);
+
+    // if I'm at the end of the file to read, write a code as last pixel to
+    // signal that the dataset is over
+    if (bytesRead < 28 * 28 * _batchSize) {
+        images[28 * 28 * _batchSize - 1] = 0xff;
+    }
+
+    return images;
+}
+
+uint8_t *readLabelBatch(size_t _batchSize, uint32_t _batchIndex,
+                        FILE *_labelFile) {
+    // allocate the array where the labels batch will be stored
+    uint8_t *labels = malloc(sizeof(uint8_t) * _batchSize);
+
+    fseek(_labelFile, LABEL_HEADER_SIZE + _batchSize * _batchIndex, SEEK_SET);
+
+    fread(labels, sizeof(uint8_t), _batchSize, _labelFile);
+
+    return labels;
 }
